134a78fc6ed9c4b433e77a2ebe19b5324886d47c
Ivan-Kukshyn <139780221+Ivan-Kukshyn@users.noreply.github.com>
2024-12-24 22:25:55 +0100

    Added plugin PHPMailer. In Contact.jsx made form. Added sendmail.php form processing script.

---
+++ sendmail.php
@@ -0,0 +1,61 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+
+// Customizing headers for CORS
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'ivan.kukshyn@gmail.com';
+        $mail->Password = 'your_password';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+        $mail->CharSet = 'UTF-8';
+        $mail->setLanguage('ru', 'phpmailer/language/');
+        $mail->isHTML(true);
+
+        // Получение данных от пользователя
+        $name = htmlspecialchars(trim($_POST['name']));
+        $email = htmlspecialchars(trim($_POST['email']));
+        $phone = htmlspecialchars(trim($_POST['phone']));
+        $subject = htmlspecialchars(trim($_POST['subject']));
+        $message = htmlspecialchars(trim($_POST['message']));
+
+        // Настройка отправителя и получателя
+        $mail->setFrom($email, $name);
+        $mail->addAddress('ivan.kukshyn@gmail.com');
+
+        // Содержание письма
+        $mail->Subject = 'New mail from Portfolio';
+        $mail->Body = "
+            <h1>New mail</h1>
+            <p><strong>Name:</strong> $name</p>
+            <p><strong>Email:</strong> $email</p>
+            <p><strong>Phone:</strong> $phone</p>
+            <h2><strong>Subject:</strong> $subject</h2>
+            <p><strong>Message:</strong> $message</p>
+        ";
+
+        // Отправка письма
+        $mail->send();
+
+        echo json_encode(['status' => 'success', 'message' => 'Email sent successfully']);
+    } catch (Exception $e) {
+        echo json_encode(['status' => 'error', 'message' => $mail->ErrorInfo]);
+    }
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
+}
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function Contact() {
+// Contact form
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    subject: '',
+    message: '',
+  });
+
+  const [reponseMessage, setReponseMessage] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8080/sendmail.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await response.json();
+      setReponseMessage(result.message);
+    } catch (error) {
+      setReponseMessage('Failed to send message.');
+    }
+  };
+
   return (
     <section className="contact" id="contact">
       <h2 className="heading">Contact Me</h2>
-      <form action="send.php" method="post">
+      <form action="sendmail.php" method="post" onSubmit={handleSubmit}>
         <div className="input-box">
-          <input type="text" name="name" placeholder="Full Name" required />
-          <input type="email" name="email" placeholder="Email Address" required />
+          <input type="text" name="name" placeholder="Full Name" value={formData.name} onChange={handleChange} required />
+          <input type="email" name="email" placeholder="Email Address" value={formData.email} onChange={handleChange} required />
         </div>
         <div className="input-box">
-          <input type="number" name="phone" placeholder="Mobile Number" />
-          <input type="text" name="subject" placeholder="Email Subject" />
+          <input type="number" name="phone" placeholder="Mobile Number" value={formData.phone} onChange={handleChange} />
+          <input type="text" name="subject" placeholder="Email Subject" value={formData.subject} onChange={handleChange} required/>
         </div>
-        <textarea name="message" cols="30" rows="10" placeholder="Your Message"></textarea>
+        <textarea name="message" cols="30" rows="10" placeholder="Your Message" value={formData.message} onChange={handleChange} required></textarea>
         <input type="submit" value="Send Message" className="btn" />
       </form>
+      {reponseMessage && <p>{reponseMessage}</p>}
     </section>
   );
 };
