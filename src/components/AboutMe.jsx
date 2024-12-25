dbb8abc7ae80c587f787ce567750329e9055a923
Ivan-Kukshyn <139780221+Ivan-Kukshyn@users.noreply.github.com>
2024-12-25 15:28:38 +0100

    Install i18n-http-backend plugin. Created i18n.js and moved in main.jsx.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,21 +9,21 @@ function AboutMe() {
 
       <div className="about-content">
         <h2 className="heading">Who i am? What i want?</h2>
-        <p className="p-1"><strong>Hello, my name is Ivan Kukshyn.</strong></p>
-        <p className="p-2">I'm 19 years old and I'm studying web and mobile development at the “La F@brique
+        <p><strong>Hello, my name is Ivan Kukshyn.</strong></p>
+        <p>I'm 19 years old and I'm studying web and mobile development at the “La F@brique
           Numérique 41” in Blois.</p>
         <br />
-        <p className="p-3">I'm from Ukraine and I'm now looking for a work-study opportunity in a company where I can
+        <p>I'm from Ukraine and I'm now looking for a work-study opportunity in a company where I can
           apply my knowledge and skills to real-life projects and gain valuable experience
           working in a team of professionals. My future goal is to become a
           qualified backend developer.</p>
         <br />
-        <p className="p-4">My current stack includes HTML, CSS, JavaScript. I'm actively learning WordPress, Bootstrap
+        <p>My current stack includes HTML, CSS, JavaScript. I'm actively learning WordPress, Bootstrap
           and
           JavaScript. I'm always looking to improve my skills. I'm willing to learn from experienced
           and contribute to the success of projects.</p>
         <br />
-        <p className="p-5">I speak many languages: <strong>English, French, Russian and Ukrainian.</strong></p>
+        <p>I speak many languages: <strong>English, French, Russian and Ukrainian.</strong></p>
       </div>
     </section>
   );
---
+++ src/i18n.js
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import HttpBackend from 'i18next-http-backend'; // To load JSON files from the server or locally.
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .use(HttpBackend)
+    .init({
+        debug: true,
+        fallbackLng: 'en',
+        backend: {
+            loadPath: 'src/assets/locales/{{lng}}.json',   
+        },
+        interpolation: {
+            escapeValue: false, //React already protects against XSS (Cross-Site Scripting)
+        }
+    });
+    
+export default i18n;
