420e7b27b3f8d69a7898e2fd9b1579d70f2b6723
Ivan-Kukshyn <139780221+Ivan-Kukshyn@users.noreply.github.com>
2024-12-26 15:48:50 +0100

    Modify Footer.jsx with language change functionality.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useTranslation } from 'react-i18next';
 
 function Footer() {
+    const { t } = useTranslation();
+
     return (
         <>
             <footer className="footer">
                 <div className="footer-text">
-                    <p>Ivan Kukshyn | <a href="/terms-of-use">Terms of Use</a></p>
+                    <p>{t('Footer.hero_name')} | <a href="/terms-of-use">{t('Footer.terms_of_use')}</a></p>
                 </div>
                 <div className="footer-iconTop">
                     <a href="#home"><i className="fa-solid fa-arrow-up"></i></a>
