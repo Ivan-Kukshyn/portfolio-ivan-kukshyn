b240dc02d8a124be42647fc7425f4aa5e4c44919
Ivan-Kukshyn <139780221+Ivan-Kukshyn@users.noreply.github.com>
2024-12-27 15:37:28 +0100

    fix: Changed App.jsx with

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import '/src/assets/css/App.css';
 import Header from '/src/components/Header';
 import Home from '/src/components/Home';
 import Footer from '/src/components/Footer';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MentionLegales from '/src/components/MentionLegales';
+import { useTranslation } from 'react-i18next';
+import i18n from './i18n';
 
 function App() {
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    if (!i18n.isInitialized) {
+      i18n.init();
+    }
+  }, [i18n]);
+
   return (
     <>
       <Header />
       <Router>
         <Routes>
-          <Route path="/" Component={Home} />
-          <Route path="/mention-legales" Component={MentionLegales} />
+          <Route path="/" element={<Home />} />
+          <Route path="/mention-legales" element={<MentionLegales />} />
         </Routes>
       </Router>
       <Footer />
