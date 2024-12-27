e06153d053252ba487a74bc6a02b662b769cd868
Ivan-Kukshyn <139780221+Ivan-Kukshyn@users.noreply.github.com>
2024-12-27 22:06:38 +0100

    Modified button in Footer.jsx.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,20 @@ import { Link } from 'react-router-dom';
 function Footer() {
     const { t } = useTranslation();
 
+    const clickToTop = () => {
+        window.scrollTo({top: 0, behavior: "smooth"});
+    }
+
     return (
         <>
             <footer className="footer">
-            <div className="footer-text">
-                <p>{t('Footer.hero_name')} | 
-                    <Link to="/mention-legales"> {t('Footer.mention_legales')}</Link>
-                </p>
-            </div>
+                <div className="footer-text">
+                    <p>{t('Footer.hero_name')} |
+                        <Link to="/mention-legales"> {t('Footer.mention_legales')}</Link>
+                    </p>
+                </div>
                 <div className="footer-iconTop">
-                    <a href="#home"><i className="fa-solid fa-arrow-up"></i></a>
+                    <a href="" onClick={clickToTop}><i className="fa-solid fa-arrow-up"></i></a>
                 </div>
             </footer>
         </>
